4a12173e0e39eae247dc9c4ded0c95fe955f2d52
Vinodh <vinodh.mahendra@gmail.com>
2020-07-18 19:33:44 +0000

    getting started project

---
+++ getting-started/src/Greeting.java
@@ -0,0 +1,6 @@
+
+public class Greeting {
+	public void greet() {
+		System.out.println("Hello Greetings!!!");
+	}
+}
---
+++ getting-started/src/TestGreeting.java
@@ -0,0 +1,10 @@
+
+public class TestGreeting {
+
+	public static void main(String[] args) {
+		Greeting hello = new Greeting();
+		hello.greet();
+
+	}
+
+}
