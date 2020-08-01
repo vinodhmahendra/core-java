54d5499e27f1c1af04580c671f03a575ba0ec330
Vinodh <vinodh.mahendra@gmail.com>
2020-08-01 17:05:23 +0000

    using static members

---
+++ bank-services-static/src/com/mybank/domain/Bank.java
@@ -0,0 +1,28 @@
+package com.mybank.domain;
+
+public class Bank {
+	private static Customer[] customers;
+	private static int numberOfCustomers;
+	
+	static {
+		customers = new Customer[10];
+		numberOfCustomers = 0;
+	}
+	
+	private Bank() {
+		
+	}
+	
+	public static void addCustomer(String f, String l) {
+		int  i  = numberOfCustomers ++;
+		customers[i] = new Customer(f, l);
+	}
+	
+	public static int getNumOfCustomers() {
+		return numberOfCustomers;
+	}
+	
+	public static Customer getCustomer(int customer_index) {
+		return customers[customer_index];
+	}
+}
